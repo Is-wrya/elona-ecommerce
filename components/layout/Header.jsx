31ee7323b7c6d92f6912fece9ebf69d912d81892
Aiswarya Kottiyal <aiswaryarameshans@gmail.com>
2026-03-14 14:33:33 +0530

    update

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -19,6 +19,7 @@ export default function Header() {
     pathname==="/addtocart"||
     pathname==="/ordersummary"||
     pathname==="/profile"||
+    pathname==="/return"||
     pathname==="/signup";
 
 const isHome = pathname === "/";
